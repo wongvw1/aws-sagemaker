f1f0901dc384a7441aa08922554ae17900f00160
Wong <voonw@f01898875ea7.ant.amazon.com>
2020-08-06 00:11:02 +0800

    updated the comments

--- notebook-lifecycle/on-start.sh
+++ notebook-lifecycle/on-start.sh
@@ -3,8 +3,9 @@
 set -e
 
 # OVERVIEW
-# This script installs a single jupyter notebook extension package in SageMaker Notebook Instance
-# For more details of the example extension, see https://github.com/jupyter-widgets/ipywidgets
+# This script downloads the ipy file into the startup folder for ipython.
+# The ipy file get executed first each time a notebook is started.
+# The ipy creates a log file and starts logging of the any code sent to the ipython kernel from the Jupyter frontend.
 
 sudo -u ec2-user -i <<'EOF'
 ipython profile create default
